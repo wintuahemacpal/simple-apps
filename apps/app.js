c209336f6d9b6a7160135c83c979723e5917c583
wintuahemacpal <w.macpal@unipa.ac.id>
2026-01-11 11:22:41 +0900

    update security

--- apps/app.js
+++ apps/app.js
@@ -20,6 +20,8 @@ app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
 
+app.disable("x-powered-by");
+
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
